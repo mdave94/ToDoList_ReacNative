97dd22e1792a2b03f9a28f0da0ecaf777ca27f57
Molnar Dávid <molnardavid2002@gmail.com>
2023-04-11 00:30:38 +0200

    Layout update with flexbox

--- App.js
+++ App.js
@@ -1,27 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,Button} from 'react-native';
+import { StyleSheet, Text, View ,Button, TextInput} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.blueBorder}>Hello World! </Text>
-  
-      <Button title='Button' /> 
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Enter your goal here !'/>
+        <Button title='Enter'/>
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text> List of Goals </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer :{
+    paddingTop: 50,
+    paddingHorizontal:16,
+    flex:1
+
   },
-  blueBorder:{
-    borderWidth:2,
-    borderColor:'blue',
-    margin:16,
-    padding:16
+  inputContainer:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    marginBottom:16,
+    borderBottomWidth:1,
+    borderBottomColor:'#cccccc',
+    flex:1,
+    
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor:'#cccccc',
+    width:'70%',
+    borderRadius:12,
+    marginRight:7,
+    padding:8,
+    
+  },
+  goalsContainer:{
+    flex:5
   }
+  
 });
