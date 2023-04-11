5f3ddae5777b037b9e7dbe6da80dadc493983bb9
Molnar Dávid <molnardavid2002@gmail.com>
2023-04-12 00:57:25 +0200

    GoalItem.js and Goalinput.js is added

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import {
   StyleSheet,
-  Text,
   View,
   Button,
   TextInput,
-  ScrollView,
   FlatList,
 } from "react-native";
 import { useState } from "react";
+import GoalItem from "./components/GoalItem";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -38,11 +37,7 @@ export default function App() {
         <FlatList
           data={goals}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem text={itemData.item.text} />
           }}
           alwaysBounceHorizontal={false}
           keyExtractor={(item, index) => {
@@ -80,14 +75,5 @@ const styles = StyleSheet.create({
   goalsContainer: {
     flex: 5,
   },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-    color: "white",
-  },
-  goalText: {
-    color: "white",
-  },
+
 });
